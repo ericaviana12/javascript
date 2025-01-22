ea79835924459f23d12e796f78244edbb6d673c5
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-01-21 21:48:16 -0300

    Atualização

---
+++ slideshow/script.js
@@ -0,0 +1,18 @@
+/**
+ * Slide show
+ * @author Erica Viana
+ */
+
+// Array (vetor) de imagens
+let slides = ["banner1.jpg","banner2.jpg","banner3.jpg"]
+let intervalo = 3000 // 3000ms = 3s (1s por imagem)
+let indice = 0 // Apoio a lógica (índice do Array)
+
+// Função para troca das imagens
+function show() {
+    // Capturar o id do banner
+    const slide = document.getElementById('slide')
+}
+
+// Executar a função ao iniciar o documento JS
+show()
