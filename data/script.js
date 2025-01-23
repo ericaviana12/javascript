51c3b10c6d6055bb68ecff475b79ad89581f6668
ERICA VIANA SOARES <erica.vsoares@senacsp.edu.br>
2025-01-23 19:28:39 -0300

    Atualização

--- data/script.js
+++ data/script.js
@@ -11,8 +11,13 @@ function obterData() {
         month: 'long',
         day: 'numeric',
     }
-    return data.toLocaleDateString('pt-br', options)
+    return `${data.toLocaleDateString('pt-br', options)} - ${data.toLocaleTimeString('pt-br')}`
 }
 
-// A linha abaixo insere na tag <p> identificadaa como dataAtual o retorno da função
-document.getElementById('dataAtual').innerHTML = obterData()
+function atualizarData() {
+    // A linha abaixo insere na tag <p> identificadaa como dataAtual o retorno da função
+    document.getElementById('dataAtual').innerHTML = obterData()
+}
+
+// Atualizar a data/hora a cada segundo
+setInterval(atualizarData, 1000) // 1000ms = 1s
